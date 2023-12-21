f937c781ea27ea6feae02628b982312769ec0ea7
Alltimers <pablowildmann@gmail.com>
2023-12-21 19:22:27 -0300

    update some lines with ES6

--- script.js
+++ script.js
@@ -26,19 +26,18 @@ function readURL(){
     }
 }
 
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector('h3');
+const [color1, color2] = document.querySelectorAll('.color');
+const body = document.querySelector('#gradient');
 
-function setGradient() {
-	body.style.background = "linear-gradient(to right, " 
-	+ color1.value + ", " + color2.value + ")";
-
-	css.textContent = body.style.background + ";";
+setGradient = () => {
+   body.style.background = linearGradient();
+   css.textContent = `${body.style.background};`;
 }
 
-
+linearGradient = () => {
+   return `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+}
 
 color1.addEventListener("input", setGradient);
 
